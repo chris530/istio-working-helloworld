e77e845a5115769c07decdf173d7141a8ad2b4ab
Chris Haessig <chaessig@demandbase.com>
2020-03-06 17:59:54 -0800

    isito upgrades

--- 5xxsample/dockerbuilds/5xxweb/server.go
+++ 5xxsample/dockerbuilds/5xxweb/server.go
@@ -2,18 +2,27 @@ package main
 
 import (
         "net/http"
+        "log"
        )
 
 func fivexx(w http.ResponseWriter, r *http.Request) {
 
-  w.WriteHeader(http.StatusServiceUnavailable)
-  w.Write([]byte("Nope"))
+  log.Print("5xx endpoint hit")
+  w.WriteHeader(http.StatusGatewayTimeout)
+  w.Write([]byte("500x error!......."))
+
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+
+  w.Write([]byte("Good"))
 
 }
 
 func main(){
 
   http.HandleFunc("/", fivexx)
-  http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/healthz", healthz)
+  http.ListenAndServe(":80", nil)
 
 }
